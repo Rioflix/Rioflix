e14c7b00f08e9d4f81d79b40b4a7a0b8d7428770
rioflix <walid.beday@gmail.com>
2021-10-31 21:58:06 +0200

    site

---
+++ learnjs/js/main.js
@@ -0,0 +1,12 @@
+let title = `JS El Zero School`,
+  dec = `Best School Ever`,
+  data = `25/10`,
+  div = `<div class="card">
+  <h3> ${title}</h3>
+  <p>${dec}</p>
+  <span>${data}</span>
+  </div>
+  `;
+document.write(div.repeat(4));
+console.log(`${title}
+${data}`)
